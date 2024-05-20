c529388ac3b54863e7cdd834057013d0413680f7
thewisepeter <rempeterwisdom@gmail.com>
2024-05-20 14:31:05 +0300

    implemented login authentification

--- expense_app/__init__.py
+++ expense_app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4fae35a28915d9bc651e0bc712350e5d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from expense_app import routes
